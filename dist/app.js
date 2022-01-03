50bd9d8f520596026e483d845b4ab32ec14c4177
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2022-01-04 01:57:45 +0800

    basic setups w/ babel

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const app = (0, _express.default)();
+const port = 3000;
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
