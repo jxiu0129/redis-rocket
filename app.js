0b9b956ee1e9adb8b28fc1b3a3046212c20c91c2
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2022-01-04 18:27:28 +0800

    add redis to rockets route

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 import express from "express";
-import redis from "redis";
+import { createClient } from "redis";
 import responseTime from "response-time";
 import axios from "axios";
 
 const app = express();
 app.use(responseTime());
 
+const redisClient = createClient({
+    host: "127.0.0.1",
+    port: 6379,
+});
+
+(async () => {
+    redisClient.on("error", (err) => console.log("Redis Client Error", err));
+    await redisClient.connect();
+})();
+
 app.route("/rockets").get(async (_, res) => {
     try {
+        const reply = await redisClient.get("rockets");
+        if (reply) {
+            console.log("using cached data");
+            res.send(JSON.parse(reply));
+            return;
+        }
         const { data } = await axios.get(
             "https://api.spacexdata.com/v3/rockets"
         );
-        // console.log(data);
+        const saveResult = await redisClient.set(
+            "rockets",
+            JSON.stringify(data),
+            { EX: 5 }
+        ); // 5 here means 5sec
+        console.log(`data cached ${saveResult}`);
         res.send(data);
     } catch (err) {
         res.send(err);
--- dist/app.js
+++ dist/app.js
@@ -8,16 +8,39 @@ var _responseTime = _interopRequireDefault(require("response-time"));
 
 var _axios = _interopRequireDefault(require("axios"));
 
+var _util = require("util");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const app = (0, _express.default)();
 app.use((0, _responseTime.default)());
-app.route("/rocket").get(async (_, res) => {
+
+const redisClient = _redis.default.createClient({
+  host: "107.0.0.1",
+  port: 6379
+});
+
+const GET_ASYNC = (0, _util.promisify)(redisClient.get).bind(redisClient);
+const SET_ASYNC = (0, _util.promisify)(redisClient.set).bind(redisClient);
+redisClient.get();
+app.route("/rockets").get(async (_, res) => {
   try {
-    const response = await _axios.default.get("https://api.spacexdata.com/v3/rockets");
-    res.send(response);
+    const reply = await GET_ASYNC("rockets");
+
+    if (reply) {
+      console.log("using cached data");
+      res.send(JSON.parse(reply));
+    }
+
+    const {
+      data
+    } = await _axios.default.get("https://api.spacexdata.com/v3/rockets");
+    const saveResult = await SET_ASYNC("rockets", JSON.stringify(data), "EX", 5); // 5 here means 5sec
+
+    console.log(`data cached ${saveResult}`);
+    res.send(data);
   } catch (err) {
-    res.send("err");
+    res.send(err);
   }
 });
 const port = 3000;
