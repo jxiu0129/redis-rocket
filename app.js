58f308acc67ff01341d1c7a72e3e36dedd723b70
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2022-01-04 17:26:12 +0800

    add rockets route

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 import express from "express";
+import redis from "redis";
+import responseTime from "response-time";
+import axios from "axios";
+
 const app = express();
+app.use(responseTime());
+
+app.route("/rockets").get(async (_, res) => {
+    try {
+        const { data } = await axios.get(
+            "https://api.spacexdata.com/v3/rockets"
+        );
+        // console.log(data);
+        res.send(data);
+    } catch (err) {
+        res.send(err);
+    }
+});
 
 const port = 3000;
 app.listen(port, () => {
--- dist/app.js
+++ dist/app.js
@@ -2,9 +2,24 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _redis = _interopRequireDefault(require("redis"));
+
+var _responseTime = _interopRequireDefault(require("response-time"));
+
+var _axios = _interopRequireDefault(require("axios"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const app = (0, _express.default)();
+app.use((0, _responseTime.default)());
+app.route("/rocket").get(async (_, res) => {
+  try {
+    const response = await _axios.default.get("https://api.spacexdata.com/v3/rockets");
+    res.send(response);
+  } catch (err) {
+    res.send("err");
+  }
+});
 const port = 3000;
 app.listen(port, () => {
   console.log("listening on port " + port);
